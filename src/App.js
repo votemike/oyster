f96bba2cd00fb5b1a09c1beb4bc79a368bbaf18a
Michael Gwynne <michael@votemike.co.uk>
2019-01-04 23:46:35 +0000

    WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Test from './Test.js';
 import './App.css';
 
 class App extends Component {
+  renderTest() {
+    return (<Test/>);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            {this.renderTest()}
         </header>
       </div>
     );
